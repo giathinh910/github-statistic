a12455ab52b0d29c6f6aa16cb7489cfce44d4a84
Bui Gia Thinh <bui.gia.thinh@sun-asterisk.com>
2019-11-11 09:28:43 +0700

    config angular for heroku deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUri: 'https://fe-github-statistic.herokuapp.com/api',
+  apiUri: 'https://github-statistic-api.herokuapp.com/api',
   clientId: '6d012bf2b7abc0e116c3'
 };
